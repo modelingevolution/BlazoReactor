89565a1daa3da1b99a9372e6e9d58b0cbb459568
Daniel Kowalski <daniel.kowalski@modelingevolution.com>
2022-11-10 17:03:49 +0100

    Add classes related to event aggregator

---
+++ Source/BlazoReactor/EventAggregator/IDelegateReference.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazoReactor.EventAggregator
+{
+    /// Represents a reference to a <see cref="Delegate"/>.
+    /// </summary>
+    public interface IDelegateReference
+    {
+        /// <summary>
+        /// Gets the referenced <see cref="Delegate" /> object.
+        /// </summary>
+        /// <value>A <see cref="Delegate"/> instance if the target is valid; otherwise <see langword="null"/>.</value>
+        Delegate Target { get; }
+    }
+    /// Represents a reference to a <see cref="Delegate"/>.
+    /// </summary>
+    public interface IDelegateReference<TDelegate> where TDelegate : Delegate
+    {
+        /// <summary>
+        /// Gets the referenced <see cref="Delegate" /> object.
+        /// </summary>
+        /// <value>A <see cref="Delegate"/> instance if the target is valid; otherwise <see langword="null"/>.</value>
+        TDelegate Target { get; }
+    }
+}
